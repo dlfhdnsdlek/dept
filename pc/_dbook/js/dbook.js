b3f6b6a2a5505775fa895919c2a62cab81328707
book design <ok@design-book.co.kr>
2023-03-15 16:25:58 +0900

    pjs header

--- pc/_dbook/js/dbook.js
+++ pc/_dbook/js/dbook.js
@@ -1,7 +1,7 @@
 $(()=>{
     $('#header .gnb .depth0 > li').on('mouseenter',function(){
-        $(this).find('ul').stop().slideDown();
+        $(this).find('ul').stop().slideDown(400);
     }).on('mouseleave', function(){
-        $(this).find('ul').stop().slideUp();
+        $(this).find('ul').stop().slideUp(400);
     });
 });
