c2238f6552f494568fd33d04a17b5fcf4228c309
book design <ok@design-book.co.kr>
2023-03-16 11:45:16 +0900

    pjs dbook js

--- pc/_dbook/js/dbook.js
+++ pc/_dbook/js/dbook.js
@@ -1,8 +1,8 @@
 $(()=>{
     $('#header .gnb .depth0 > li').on('mouseenter',function(){
-        $(this).addClass('on').find('ul').stop().slideDown(400);
+        $(this).addClass('on').find('ul').stop().fadeIn();
     }).on('mouseleave', function(){
-        $(this).removeClass('on').find('ul').stop().slideUp(400);
+        $(this).removeClass('on').find('ul').stop().fadeOut();
     });
 
     $('#header').on('mouseenter',function(){
