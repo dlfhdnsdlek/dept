0296f4812524672a12b987b3d3006dcd64bad045
book design <ok@design-book.co.kr>
2023-03-20 14:25:16 +0900

    pjs collabo

--- pc/_dbook/js/dbook.js
+++ pc/_dbook/js/dbook.js
@@ -1,18 +1,26 @@
 $(()=>{
+    /* 상단 카테고리 하위 메뉴 */
     $('#header .gnb .depth0 > li').on('mouseenter',function(){
         $(this).addClass('on').find('ul').stop().slideDown(200);
     }).on('mouseleave', function(){
         $(this).removeClass('on').find('ul').stop().slideUp(200);
     });
 
+    /* 상단 마우스 오버시 */
     $('#header').on('mouseenter',function(){
         $(this).addClass('active');
     }).on('mouseleave', function(){
         $(this).removeClass('active');
     });
 
+    /* 종료된 공구 페이지 */
     var frmPathSearch = location.pathname + location.search;
     if ( frmPathSearch == '/pages/product/list.html?categoryNo=286231') {
         $('.sub_content').addClass('collabo-end');
     }
+    var regexp = /\B(?=(\d{3})+(?!\d))/g;
+    var price = parseFloat($('.cont-box .item_list_type .item_money_box .item_price b').text().replace(/,/g, ""));
+    var customPrice = parseFloat($('.cont-box .item_list_type .item_money_box del').text().replace(/,/g, ""));
+    var saleRate = (((customPrice - price) / customPrice) * 100).toFixed(0);
+    $('.cont-box .item_list_type .item_money_box .item_price').prepend( '<span class="sale-rate">' + saleRate.replace(regexp, ',') + '%</spna>');
 });
