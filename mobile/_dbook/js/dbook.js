977683d7d08d1300f780156c7c9efa97fca61dfd
book design <ok@design-book.co.kr>
2023-03-23 10:08:12 +0900

    pjs js

--- mobile/_dbook/js/dbook.js
+++ mobile/_dbook/js/dbook.js
@@ -5,7 +5,6 @@ $(()=>{
     if ( frmSearch.match('categoryNo=286231') ) {
         $('.sub_content').addClass('collabo-end');
     }
-
     $('#category-navigation .category_side > li').each(function(){
         var eCategoryNo = $(this).attr('data-categoryno');
         if (eCategoryNo == '286231') {
