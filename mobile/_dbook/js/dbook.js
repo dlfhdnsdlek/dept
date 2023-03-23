91c25f0919b01792fe1332fea1942eec31912ecd
book design <ok@design-book.co.kr>
2023-03-23 10:02:18 +0900

    pjs js

--- mobile/_dbook/js/dbook.js
+++ mobile/_dbook/js/dbook.js
@@ -1,10 +1,10 @@
 $(()=>{
 
     /* 종료된 공구 페이지 */
-    var frmPathSearch = location.pathname + location.search;
-    if ( frmPathSearch == '/pages/product/list.html?categoryNo=286231') {
+    var frmSearch = location.search;
+    if ( frmSearch.match('categoryNo=286231') ) {
         $('.sub_content').addClass('collabo-end');
-    }    
+    }
 
     $('#category-navigation .category_side > li').each(function(){
         var eCategoryNo = $(this).attr('data-categoryno');
--- pc/_dbook/js/dbook.js
+++ pc/_dbook/js/dbook.js
@@ -28,8 +28,8 @@ $(()=>{
     });
 
     /* 종료된 공구 페이지 */
-    var frmPathSearch = location.pathname + location.search;
-    if ( frmPathSearch == '/pages/product/list.html?categoryNo=286231') {
+    var frmSearch = location.search;
+    if ( frmSearch.match('categoryNo=286231') ) {
         $('.sub_content').addClass('collabo-end');
     }
 
